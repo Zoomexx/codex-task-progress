c5f7c96ffb7b658d6c540f14a6c981c97fb0c555
Zoomexx <zoome0037@gmail.com>
2026-09-15 15:27:43 +0800

    docs: publish v0.4.4 approval visibility release
    Document the v0.4.4 pending-approval priority, call_id-safe clearing, and 2-second Computer Use detection window.

--- build-app.sh
+++ build-app.sh
@@ -35,9 +35,9 @@ cat > "$contents_dir/Info.plist" <<'PLIST'
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>0.4.3</string>
+    <string>0.4.4</string>
     <key>CFBundleVersion</key>
-    <string>7</string>
+    <string>8</string>
     <key>LSUIElement</key>
     <true/>
 </dict>
